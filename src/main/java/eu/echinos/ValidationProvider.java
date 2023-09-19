c0a8d1ef41ed4392f1787a47fb7bb31fb6545ebe
patrick-werner <pa.f.werner@gmail.com>
2023-09-19 15:45:55 +0200

    added oackage to classpath

--- src/main/java/eu/echinos/ValidationProvider.java
+++ src/main/java/eu/echinos/ValidationProvider.java
@@ -3,14 +3,16 @@ package eu.echinos;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.validation.FhirValidator;
+import java.io.IOException;
 import org.hl7.fhir.common.hapi.validation.support.CommonCodeSystemsTerminologyService;
 import org.hl7.fhir.common.hapi.validation.support.InMemoryTerminologyServerValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.NpmPackageValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.SnapshotGeneratingValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 
 public class ValidationProvider extends FhirValidator {
 
-  public ValidationProvider(FhirContext ctx) {
+  public ValidationProvider(FhirContext ctx) throws IOException {
     super(ctx);
 
     // Create a chain that will hold our modules
@@ -26,5 +28,10 @@ public class ValidationProvider extends FhirValidator {
     supportChain.addValidationSupport(new InMemoryTerminologyServerValidationSupport(ctx));
     //snapshot generation
     supportChain.addValidationSupport(new SnapshotGeneratingValidationSupport(ctx));
+
+    NpmPackageValidationSupport npmPackageSupport = new NpmPackageValidationSupport(ctx);
+    npmPackageSupport.loadPackageFromClasspath("classpath:package/de.gematik.elektronische-versicherungsbescheinigung-1.0.0-rc3.tgz");
+
+
   }
 }
