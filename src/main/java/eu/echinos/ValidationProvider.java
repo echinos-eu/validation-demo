d5e84fa2f0f31bb6452f1fd1c02160c9724b4e0c
patrick-werner <pa.f.werner@gmail.com>
2023-09-19 15:56:39 +0200

    working

--- src/main/java/eu/echinos/ValidationProvider.java
+++ src/main/java/eu/echinos/ValidationProvider.java
@@ -2,7 +2,9 @@ package eu.echinos;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.ValidationResult;
 import java.io.IOException;
 import org.hl7.fhir.common.hapi.validation.support.CachingValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.CommonCodeSystemsTerminologyService;
@@ -11,12 +13,23 @@ import org.hl7.fhir.common.hapi.validation.support.NpmPackageValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.SnapshotGeneratingValidationSupport;
 import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleType;
 
 public class ValidationProvider extends FhirValidator {
 
   public static void main(String[] args) throws IOException {
-    ValidationProvider val = new ValidationProvider(FhirContext.forR4());
+    FhirContext ctx = FhirContext.forR4();
+    IParser iParser = ctx.newJsonParser().setPrettyPrint(true);
+    ValidationProvider val = new ValidationProvider(ctx);
+    Bundle bundle = new Bundle();
+    bundle.setType(BundleType.MESSAGE);
+    bundle.getMeta().addProfile("https://gematik.de/fhir/eeb/StructureDefinition/EEBAnfrageBundle");
+    ValidationResult validationResult = val.validateWithResult(bundle);
+    System.out.println(iParser.encodeResourceToString(validationResult.getOperationOutcome()));
+
   }
+
   public ValidationProvider(FhirContext ctx) throws IOException {
     super(ctx);
 
@@ -35,7 +48,8 @@ public class ValidationProvider extends FhirValidator {
     supportChain.addValidationSupport(new SnapshotGeneratingValidationSupport(ctx));
 
     NpmPackageValidationSupport npmPackageSupport = new NpmPackageValidationSupport(ctx);
-    npmPackageSupport.loadPackageFromClasspath("classpath:package/de.gematik.elektronische-versicherungsbescheinigung-1.0.0-rc3.tgz");
+    npmPackageSupport.loadPackageFromClasspath(
+        "classpath:package/de.gematik.elektronische-versicherungsbescheinigung-1.0.0-rc3.tgz");
     supportChain.addValidationSupport(npmPackageSupport);
 
     // Wrap the chain in a cache to improve performance
